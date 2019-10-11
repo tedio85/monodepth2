452a52a5b7a057fa8861871e496352fb7e24a8ff
jamgirrafe <jamgirrafe@gmail.com>
2019-10-11 10:33:24 -0700

    skip frame 3032

--- tests/aggregated_loss_curve/agg_loss_curve.py
+++ tests/aggregated_loss_curve/agg_loss_curve.py
@@ -461,11 +461,12 @@ if __name__ == '__main__':
     for i, frame_t in enumerate(partition, start):
         frame = load_data(frame_t, root, img_w, img_h)
         frame = data_to_device_tensor(frame)
-        agg_results = run_test(frame_t,
-                               frame,
-                               layers,
-                               psize_list,
-                               deviation, step, root, args.type_name, n_samples=n_samples)
+        if i != 3032:
+            agg_results = run_test(frame_t,
+                                   frame,
+                                   layers,
+                                   psize_list,
+                                   deviation, step, root, args.type_name, n_samples=n_samples)
         if i % log_iter == 0:
             toc = time.time()
             print('Frame {}/{}, {}s/it'.format(i+1, len(frame_list), (toc-tic)/log_iter))
